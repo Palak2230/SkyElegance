ae8cbd043c7c71d5bed838adf2bc03589eaaa8ff
Palak2230 <109944418+Palak2230@users.noreply.github.com>
2023-09-01 09:36:41 +0530

    alert message for invalid city
    used error handling to display alert message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './services/weather.service';
 import { WeatherData } from './models/weather.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-   constructor (private weatherService : WeatherService){
-this.getWeatherData(this.cityName);
-    }
+  constructor(private weatherService: WeatherService) {
+    this.getWeatherData(this.cityName);
+  }
 
-    weatherData ?: WeatherData;
-    cityName  :string="Jaipur" ;
+  weatherData?: WeatherData;
+  cityName: string = "Jaipur";
+ 
   ngOnInit(): void {
-     
+
   }
-  city:string='Jaipur';
-  onSubmit (){
-this.getWeatherData(this.cityName);
-this.city=this.cityName;
-this.cityName='';
+  city: string = 'Jaipur';
+  iserror: boolean = false;
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    if (this.iserror) this.iserror = false;
   }
- private  getWeatherData(cityName:string)
- {this.weatherService.getWeatherData(cityName)
- .subscribe({
-   next : (response)=>{
-     this.weatherData=response;
-   }
- });
-}}
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          this.city = cityName;
+          this.cityName = '';
+        },
+        error: (error) => {
+          alert('Please enter a valid city !!');
+          this.cityName = '';
+          this.iserror = true;
+        }
+      });
+
+  }
+}
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,22 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { WeatherData } from '../models/weather.model';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName:string): Observable<WeatherData> {
-return this.http.get<WeatherData>(environment.weatherApiBaseUrl,{
-  headers:new HttpHeaders()
-  .set(environment.XRapidAPIHostHeaderName,environment.XRapidAPIHostHeaderValue)
-  .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
-  params : new HttpParams()
-  .set('city',cityName)
-  .set('units','standard')
- 
-})
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+        .set('city', cityName)
+        .set('units', 'standard')
+
+    }
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errormessage = ''
+    if (error.status === 0) {
+
+      console.error('An error occurred:', error.error);
+    } else {
+
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+      errormessage = `Backend returned code ${error.status}, body was: `, error.error;
+    }
+
+    errormessage += 'Something bad happened; please try again later.';
+    return throwError(() => new Error(errormessage));
   }
 }
